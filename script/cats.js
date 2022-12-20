7f2e165abc69c76647d303bb05411e553170e156
Katy2989 <katusa00@mail.ru>
2022-12-20 19:44:24 +0600

    first commit

---
+++ script/cats.js
@@ -0,0 +1,92 @@
+ const cats = [
+    {
+        "id": 3953988569,
+        "name": "Марсик",
+        "age": 0,
+        "description": "Мягкий и пушистый",
+        "rate": 1,
+        "favorite": true,
+        "image": "https://mobimg.b-cdn.net/v3/fetch/c4/c493aac67877288476b0fc52d55f55cf.jpeg"
+    },
+    {
+        "id": 3953988570,
+        "name": "Барсик",
+        "favorite": true,
+        "rate": 10,
+        "age": 10,
+        "description": "Старый кот",
+        "image": "https://haski-mana.ru/wp-content/uploads/2/9/d/29d3aa6b462bf0e8716c97f6f4328520.jpeg"
+    },
+    {
+        "id": 3953988571,
+        "name": "Жирок",
+        "favorite": true,
+        "rate": 10,
+        "age": 7,
+        "description": "Жирный кот",
+        "image": "https://gomeovet.ru/images/articles/Ноябрь%2021/ozhirenie_u_sobak_i_koshek.jpg"
+    },
+    {
+        "id": 3953988572,
+        "name": "Волосатик",
+        "favorite": true,
+        "rate": 10,
+        "age": 5,
+        "description": "Волосатый и дерзкий кот",
+        "image": "https://fun-cats.ru/wp-content/uploads/d/c/2/dc26e77008f992a9da48469d261edf0e.jpeg"
+    },
+    {
+        "id": 3953988573,
+        "name": "Врединка",
+        "favorite": true,
+        "rate": 5,
+        "age": 1,
+        "description": "Маленький, вредный кот",
+        "image": "https://www.sunny-cat.ru/datas/users/1-centurion029.jpg"
+    },
+    {
+        "id": 3953988574,
+        "name": "Страшилка",
+        "favorite": false,
+        "rate": 9,
+        "age": 2,
+        "description": "Страшненький и опасненький котик",
+        "image": "https://s7.hostingkartinok.com/uploads/images/2015/06/1729b76e15613b96bcad0858122af285.jpg"
+    },
+    {
+        "id": 3953988575,
+        "name": "Толстяш",
+        "favorite": false,
+        "rate": 10,
+        "age": 4,
+        "description": "Кот, который любит много есть и спать",
+        "image": "https://image3.thematicnews.com/uploads/topics/preview/00/07/05/86/c12af76f92.jpg"
+    },
+    {
+        "id": 3953988576,
+        "name": "Пушистик",
+        "favorite": true,
+        "rate": 10,
+        "age": 4,
+        "description": "Пушистый и серьезный кот",
+        "image": "https://img.huffingtonpost.com/asset/5e5ab176230000ca0bdddc7e.jpeg?cache=CHPYzTW2pW&amp;ops=1778_1000"
+    },
+    {
+        "id": 3753988577,
+        "name": "Снежный барс",
+        "favorite": false,
+        "rate": 2,
+        "age": 2,
+        "description": "Белый и опасный",
+        "image": "https://99px.ru/sstorage/53/2018/06/mid_230568_894055.jpg"
+    },
+    {
+        "id": 3753988579,
+        "name": "Милашка",
+        "favorite": true,
+        "rate": 10,
+        "age": 0,
+        "description": "Мелкий и игривый котик",
+        "image": "https://shutniks.com/wp-content/uploads/2019/11/milye_kotyata_i_smeshnye_32_30114121.jpg"
+    }
+]
---
+++ script/index.js
@@ -0,0 +1,58 @@
+// import {cats} from "./cats";
+
+const cardsContainer = document.querySelector('.cards');
+const btnOpenPopupForm = document.querySelector('#add');
+const formCatAdd = document.querySelector('#popup-form-cat');
+
+
+const popupAddCat = new Popup('popup-add-cats');
+popupAddCat.setEventListener();
+
+btnOpenPopupForm.addEventListener('click', () => {
+  popupAddCat.open();
+});
+
+
+cats.forEach(function (catData) {
+  const cardInstance = new Card(catData, '#card-template');
+  const newCardElement = cardInstance.getElement();
+  cardsContainer.append(newCardElement);
+});
+
+console.log(cardsContainer);
+
+formCatAdd.addEventListener('submit', handleFormAddCat);
+
+function serializeForm(elements) {
+const formData={};
+
+elements.forEach((input)=>{
+if(input.type === 'submit') {} return;
+
+if (input.type !== "checkbox") {
+  formData[input.name] = input.value;
+
+}
+if (input.type === "checkbox") {
+  formData[input.name]=input.checked;
+}
+});
+return formData;
+}
+
+function handleFormAddCat(e) 
+{
+  e.preventDefault();
+  const elementsFormCat = [...formCatAdd.elements];
+
+  const dataFromForm = serializeForm(elementsFormCat);
+  console.log(dataFromForm);
+
+  const cardInstance = new Card(dataFromForm, '#card-template');
+  const newCardElement = cardInstance.getElement();
+ cardsContainer.append(newCardElement);
+
+  console.log(elementsFormCat);
+
+  popupAddCat.close();
+}
